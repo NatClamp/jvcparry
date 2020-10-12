56a1fe64fd2dd0672a8ea61c4478eea481aef9b8
NatClamp <nat.clamp@gmail.com>
2020-10-12 14:17:01 +0100

    :iphone: making products page responsive

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -17,30 +17,28 @@ const ProductsPage = () => {
 
   if (!products) return <Loading />
   return (
-    <Container>
-      <Row>
-        {products.map(product => (
-          <Col key={product.id} size="3" >
-            <Link to={`/product/${product.id}`} style={{ textDecoration: 'none' }}>
-              <Div p="2rem">
-                <Div
-                  h="20rem"
-                  bgImg={product.images[0].src}
-                  bgSize="cover"
-                  bgPos="center center"
-                  shadow="3"
-                  hoverShadow="4"
-                  transition="0.3s"
-                  m={{ b: "1.5rem" }}
-                >
-                </Div>
-                <Text tag="h1" textWeight="300" textSize="subheader" textDecor="none" textColor="black500">{product.title}</Text>
-                <Text tag="h2" textWeight="300" textSize="body" textDecor="none" textColor="gray500">${product.variants[0].price}</Text>
+    <Container d='flex' flexDir={{ xs: 'column', md: 'row' }} flexWrap="wrap">
+      {products.map(product => (
+        <Col key={product.id} size={{ xs: '12', md: '4' }} >
+          <Link to={`/product/${product.id}`} style={{ textDecoration: 'none' }}>
+            <Div p="2rem">
+              <Div
+                h="20rem"
+                bgImg={product.images[0].src}
+                bgSize="cover"
+                bgPos="center center"
+                shadow="3"
+                hoverShadow="4"
+                transition="0.3s"
+                m={{ b: "1.5rem" }}
+              >
               </Div>
-            </Link>
-          </Col>
-        ))}
-      </Row>
+              <Text tag="h1" textWeight="300" textSize="subheader" textDecor="none" textColor="black500">{product.title}</Text>
+              <Text tag="h2" textWeight="300" textSize="body" textDecor="none" textColor="gray500">${product.variants[0].price}</Text>
+            </Div>
+          </Link>
+        </Col>
+      ))}
     </Container>
   )
 }
