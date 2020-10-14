fc871a8301a2b23948b1e2d3a7a85027392b167d
NatClamp <nat.clamp@gmail.com>
2020-10-14 15:25:45 +0100

    :iphone: add mobile layout to product page

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,37 +1,33 @@
 import React, { useEffect, useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/shopContext'
-import { Text, Div, Button, Row, Col, Container } from 'atomize'
+import { Text, Button, Row, Col, Container, Icon, Anchor } from 'atomize'
 import Loading from '../components/Loading'
 
 const ProductPage = () => {
     let { id } = useParams()
     const { fetchProductWithId, addItemToCheckout, product } = useContext(ShopContext)
-    // const [product, setProduct] = useState(null)
-    // async function fetchData() {
-    //     const fetchedProduct = await fetchProductWithId(id)
-    //     setProduct(fetchedProduct)
-    // }
+
 
     useEffect(() => {
         fetchProductWithId(id)
-        
-        // fetchData()
         return () => {
-            // setProduct(null)
         };
-    }, [ fetchProductWithId, id])
+    }, [fetchProductWithId, id])
 
     if (!product.title) return <Loading />
     return (
         <Container>
-            <Row m={{ b: "2rem" }} p="2rem">
-                <Col>
-                    <Div bgImg={product.images[0].src} shadow="3" bgSize="cover" w="100%" bgPos="center center" h="40rem"/>
+            <Row d='flex' justify='flex-start' align='center' p={{ t: '20px', l: '10px' }}>
+                <Icon name="Back" size="20px" color="gray900" /> <Anchor href='/products' textColor="gray900" textSize='body' >Back to products</Anchor>
+            </Row>
+            <Row m={{ b: "2rem" }} p="2rem" d='flex' justify={{ xs: 'center', sm: 'space-around' }}>
+                <Col size={{ xs: '12', sm: '4' }} d='flex' justify='center' align='center'>
+                    <img src={product.images[0].src} alt='' style={{ maxWidth: '350px' }} />
                 </Col>
-                <Col>
+                <Col size={{ xs: '10', sm: '6' }}>
                     <Text tag="h1" textColor="black500" textWeight="200" m={{ y: '2rem' }}>{product.title}</Text>
-                    <Text tag="h3" m={{ y: '2rem' }} textWeight="200">${product.variants[0].price}</Text>
+                    <Text tag="h3" m={{ y: '2rem' }} textWeight="200">{product.variants[0].price} {product.variants[0].priceV2.currencyCode}</Text>
                     <Text tag="p" textSize="paragraph" textColor="gray900" textWeight="200">{product.description}</Text>
                     <Button rounded="0" shadow="3" bg="black500" m={{ y: '2rem' }} onClick={() => addItemToCheckout(product.variants[0].id, 1)}>Add To Cart</Button>
                 </Col>
