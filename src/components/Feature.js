bfb1b8dd5279d8dfe520602af2634de1856ea9a5
Natalie Clamp <natalieclamp@natalies-air-2.home>
2020-12-22 18:41:59 +0000

    :sparkles: add new product section and swap out slider image

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -6,8 +6,7 @@ import 'pure-react-carousel/dist/react-carousel.es.css';
 import CftDSlider from '../images/CftD_Slider.jpg';
 import DRSlider from '../images/DR_Slider.jpg';
 import DrakarSlider from '../images/Drakar_Slider.jpg';
-// import GrimmSlider from '../images/Grimm_Slider.jpg';
-import TDRSlider from '../images/TDR_Slider.jpg';
+import SantaSlider from '../images/Santa_Slider.jpg';
 import XmasSlider from '../images/Christmas_Campaign_Bundle_Slider.jpg';
 
 const Feature = () => {
@@ -27,8 +26,8 @@ const Feature = () => {
             <Slide index="0"><Anchor target="_blank" href='https://www.dmsguild.com/product/280922/Call-from-the-Deep?affiliate_rem=819295'><img src={CftDSlider} alt="Call from the deep description" /></Anchor></Slide>
             <Slide index="1"><Anchor target="_blank" href='https://www.dmsguild.com/product/323832/Dragon-Relics?affiliate_rem=819295'><img src={DRSlider} alt="Dragon Relics description" /></Anchor></Slide>
             <Slide index="2"><Anchor target="_blank" href='https://www.kickstarter.com/projects/jvcparry/drakar'><img src={DrakarSlider} alt="Drakar description" /></Anchor></Slide>
-            <Slide index="3"><Anchor target="_blank" href='https://www.dmsguild.com/product/337907/JVC-Campaign-Bundle-2020-BUNDLE?affiliate_id=819295&fbclid=IwAR2BsGHrNbJDKEW_kvKisF-D3Zxr7QOIombfqAxP5YXGKphLFzb27ZcUKMc'><img src={XmasSlider} alt="Grimm Encounters description" /></Anchor></Slide>
-            <Slide index="4"><Anchor target="_blank" href='https://jvcparry.itch.io/the-dead-rise'><img src={TDRSlider} alt="The dead rise description" /></Anchor></Slide>
+            <Slide index="3"><Anchor target="_blank" href='https://www.dmsguild.com/product/337907/JVC-Campaign-Bundle-2020-BUNDLE?affiliate_id=819295&fbclid=IwAR2BsGHrNbJDKEW_kvKisF-D3Zxr7QOIombfqAxP5YXGKphLFzb27ZcUKMc'><img src={XmasSlider} alt="Christmas bundle description" /></Anchor></Slide>
+            <Slide index="4"><Anchor target="_blank" href='/products#driveThruProducts'><img src={SantaSlider} alt="Santa's little helpers description" /></Anchor></Slide>
           </Slider>
           <ButtonBack className='slider__button slider__button--back'><Icon name="Back" color="black" size="20px" /></ButtonBack>
           <ButtonNext className='slider__button slider__button--next'><Icon name="Next" color="black" size="20px" /></ButtonNext>
