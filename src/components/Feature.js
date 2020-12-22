2611f4f8200445914e528de3406c4a68cf781bae
NatClamp <nat.clamp@gmail.com>
2020-12-22 18:49:38 +0000

    :bug: remove _blank on in-site link

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -27,7 +27,7 @@ const Feature = () => {
             <Slide index="1"><Anchor target="_blank" href='https://www.dmsguild.com/product/323832/Dragon-Relics?affiliate_rem=819295'><img src={DRSlider} alt="Dragon Relics description" /></Anchor></Slide>
             <Slide index="2"><Anchor target="_blank" href='https://www.kickstarter.com/projects/jvcparry/drakar'><img src={DrakarSlider} alt="Drakar description" /></Anchor></Slide>
             <Slide index="3"><Anchor target="_blank" href='https://www.dmsguild.com/product/337907/JVC-Campaign-Bundle-2020-BUNDLE?affiliate_id=819295&fbclid=IwAR2BsGHrNbJDKEW_kvKisF-D3Zxr7QOIombfqAxP5YXGKphLFzb27ZcUKMc'><img src={XmasSlider} alt="Christmas bundle description" /></Anchor></Slide>
-            <Slide index="4"><Anchor target="_blank" href='/products#driveThruProducts'><img src={SantaSlider} alt="Santa's little helpers description" /></Anchor></Slide>
+            <Slide index="4"><Anchor href='/products#driveThruProducts'><img src={SantaSlider} alt="Santa's little helpers description" /></Anchor></Slide>
           </Slider>
           <ButtonBack className='slider__button slider__button--back'><Icon name="Back" color="black" size="20px" /></ButtonBack>
           <ButtonNext className='slider__button slider__button--next'><Icon name="Next" color="black" size="20px" /></ButtonNext>
