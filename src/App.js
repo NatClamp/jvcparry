74634f526a8155285dbba8179b7d84aa77c7c6c7
NatClamp <nat.clamp@gmail.com>
2021-03-08 19:25:10 +0000

    :chart_with_upwards_trend: use react-router-ga to track pages

--- src/App.js
+++ src/App.js
@@ -1,74 +1,62 @@
-import React from 'react';
-import './App.css';
-import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
-import { Client as Styletron } from "styletron-engine-atomic";
-import { BrowserRouter as Router, Switch, Route, useRouteMatch } from "react-router-dom";
-import { createBrowserHistory } from 'history';
-import ReactGA from 'react-ga';
+import React from "react";
+import "./App.css";
+import {
+  Switch,
+  Route,
+  useRouteMatch,
+} from "react-router-dom";
 
-import HomePage from './pages/HomePage';
-import ProductsPage from './pages/ProductsPage';
-import DMsProductPage from './pages/DMsProductPage';
-import ProductPage from './pages/ProductPage';
-import HireMe from './pages/HireMePage';
-import Blog from './pages/BlogPage';
-import BlogPost from './pages/BlogPostPage';
-import Header from './components/Header';
-import HomeHero from './components/HomeHero';
-import Cart from './components/Cart';
-import Footer from './components/Footer';
-import ScrollToTop from './components/ScrollToTop';
-import PageNotFound from './pages/404';
-import IndieProductsPage from './pages/IndieProductsPage';
-import Modal from './components/SubscribeModal';
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import DMsProductPage from "./pages/DMsProductPage";
+import ProductPage from "./pages/ProductPage";
+import HireMe from "./pages/HireMePage";
+import Blog from "./pages/BlogPage";
+import BlogPost from "./pages/BlogPostPage";
+import Header from "./components/Header";
+import HomeHero from "./components/HomeHero";
+import Cart from "./components/Cart";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
+import PageNotFound from "./pages/404";
+import IndieProductsPage from "./pages/IndieProductsPage";
+import Modal from "./components/SubscribeModal";
 
-ReactGA.initialize('UA-191601925-1');
-const history = createBrowserHistory();
-history.listen(location => {
-  ReactGA.set({ page: location.pathname });
-  ReactGA.pageview(location.pathname);
-});
-
-
-const debug = process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
-const engine = new Styletron();
 
 const App = () => {
   return (
-        <StyletronProvider value={engine} debug={debug} debugAfterHydration>
-          <Router history={history}>
-            <ScrollToTop />
-            <Cart />
-            <Switch>
-              <Route exact path="/product/:id">
-                <Header />
-                <ProductPage />
-              </Route>
-              <Route path="/products">
-                <NestedProducts />
-              </Route>
-              <Route path="/hire-me">
-                <Header />
-                <HireMe />
-              </Route>
-              <Route path="/blog">
-                <NestedBlogs />
-              </Route>
-              <Route path="/" exact>
-                <Modal/>
-                <HomeHero />
-                <HomePage />
-              </Route>
-              <Route>
-                <Header />
-                <PageNotFound />
-              </Route>
-            </Switch>
-            <Footer />
-          </Router>
-        </StyletronProvider>
+    <React.Fragment>
+      <ScrollToTop />
+      <Cart />
+      <Switch>
+        <Route exact path="/product/:id">
+          <Header />
+          <ProductPage />
+        </Route>
+        <Route path="/products">
+          <NestedProducts />
+        </Route>
+        <Route path="/hire-me">
+          <Header />
+          <HireMe />
+        </Route>
+        <Route path="/blog">
+          <NestedBlogs />
+        </Route>
+        <Route path="/" exact>
+          <Modal />
+          <HomeHero />
+          <HomePage />
+        </Route>
+        <Route>
+          <Header />
+          <PageNotFound />
+        </Route>
+      </Switch>
+      <Footer />
+    </React.Fragment>
   );
-}
+};
 
 const NestedProducts = () => {
   let { path } = useRouteMatch();
@@ -88,8 +76,8 @@ const NestedProducts = () => {
         <IndieProductsPage />
       </Route>
     </Switch>
-  )
-}
+  );
+};
 
 const NestedBlogs = () => {
   let { path } = useRouteMatch();
@@ -105,7 +93,7 @@ const NestedBlogs = () => {
         <BlogPost />
       </Route>
     </Switch>
-  )
-}
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import ShopProvider from './context/shopContext';
-import BlogProvider from './context/blogContext';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider, DebugEngine } from "styletron-react"
+import { BrowserRouter as Router } from "react-router-dom";
+import Analytics from "react-router-ga";
 
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import ShopProvider from "./context/shopContext";
+import BlogProvider from "./context/blogContext";
+
+const debug =
+  process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
+const engine = new Styletron();
 
 ReactDOM.render(
-    <ShopProvider>
-        <BlogProvider>
+  <ShopProvider>
+    <BlogProvider>
+      <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+        <Router>
+          <Analytics id="UA-191601925-1" debug>
             <App />
-        </BlogProvider>
-    </ShopProvider>, 
-    document.getElementById('root'));
+          </Analytics>
+        </Router>
+      </StyletronProvider>
+    </BlogProvider>
+  </ShopProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
