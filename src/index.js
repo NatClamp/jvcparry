409db4bd08db8c720eb0a5681f3a5143da5182ec
NatClamp <nat.clamp@gmail.com>
2021-05-19 11:33:34 +0100

    :fire: remove service worker

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import Analytics from 'react-router-ga';
 
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import ShopProvider from './context/shopContext';
 import BlogProvider from './context/blogContext';
 
@@ -28,8 +27,3 @@ ReactDOM.render(
   </ShopProvider>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
